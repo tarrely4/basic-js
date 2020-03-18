41084a4765ef123d7ef073b341947d359ece4f68
tarr <tarr.mail@yandex.ru>
2020-03-18 18:29:46 +0300

    countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let number = 0;
+  matrix.forEach((line) => {
+    line.forEach((el) => {
+      number += (el == '^^') ? 1 : 0;
+    })
+  })
+  return number;
 };
