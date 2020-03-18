37ad8ff51a850fcf12ef8222e473faa5ec49b124
tarr <tarr.mail@yandex.ru>
2020-03-18 21:40:15 +0300

    dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let result = '';
+  members.forEach((el) => {(typeof el == 'string') ? result += el.trim().toUpperCase()[0] : result += ''});
+  return  String(result.split('').sort().join(''));
 };
