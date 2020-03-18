59323d92ed9471f14e27d0026367aa9b9a17c524
tarr <tarr.mail@yandex.ru>
2020-03-18 23:29:01 +0300

    what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (date.prototype !== new Date().prototype || !date.getUTCDate()) throw new Error();
+  switch (date.getMonth()) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+      break;
+  }
 };
