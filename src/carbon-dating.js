8e663daca6038cb7772a1dd67e7c7e3c201a9eee
tarr <tarr.mail@yandex.ru>
2020-03-18 21:09:08 +0300

    carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let sampleActivityNumber = parseFloat(sampleActivity);
+  if (sampleActivityNumber <= 0 || sampleActivityNumber >= MODERN_ACTIVITY || typeof sampleActivity != 'string' || !isFinite(sampleActivityNumber)) return false;
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = (Math.log(MODERN_ACTIVITY / sampleActivityNumber)) / k;
+  return Math.ceil(t);
 };
