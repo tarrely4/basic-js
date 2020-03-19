6a1e8e1641a131d2a2809a569cae44395ed175b0
tarr <tarr.mail@yandex.ru>
2020-03-19 11:38:39 +0300

    transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,23 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr) || !arr) throw new Error();
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+        case `--discard-next`:
+          i++;
+          break;
+        case `--discard-prev`:
+          if (result.length) result.pop();
+          break;
+        case `--double-next`:
+          if (i < arr.length - 1) result.push(arr[i + 1]);
+          break;
+        case `--double-prev`:
+          if (i) result.push(arr[i - 1]);
+          break;
+        default:
+          result.push(arr[i]);
+      }
+  }
+  return result;
 };
