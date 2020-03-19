51d65279ce4df63c206f6d48faac44743fd90303
tarr <tarr.mail@yandex.ru>
2020-03-19 17:54:35 +0300

    recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,9 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+      let depth = 0;
+      arr.forEach((el) => {
+        depth = Array.isArray(el) ? Math.max(this.calculateDepth(el), depth) : depth;
+      })
+      return ++depth;
     }
 };
