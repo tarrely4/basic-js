9fb593855129c56bc65ae6310a2dafd6e740bd2d
tarr <tarr.mail@yandex.ru>
2020-03-20 11:00:04 +0300

    extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str += '';
+  let repeatTimes = 0 || options.repeatTimes;
+  let separator = ('separator' in options) ? options.separator : '+';
+  let addition = '' + options.addition;
+  let additionRepeatTimes = 0 || options.additionRepeatTimes;
+  let additionSeparator = ('additionSeparator' in options) ? options.additionSeparator : '|';
+  let result = '';
+  while (additionRepeatTimes) {
+    str += addition;
+    str += additionRepeatTimes > 1 ? additionSeparator : '';
+    additionRepeatTimes--
+  }
+  while (repeatTimes) {
+    result += str;
+    result += repeatTimes > 1 ? separator : '';
+    repeatTimes--
+  }
+  return result || str + addition;
 };
-  
